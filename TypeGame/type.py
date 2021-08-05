5b583d2d29e9738efb35cac98cea38c396c1ab67
mattcodesz <m.rickma14@gmail.com>
2021-08-05 15:23:19 -0400

    first commit

---
+++ TypeGame/type.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import *
+import sys
+import time
+import random
+
+class Game:
+    #initialize variables for the speed test
+    def __init__(self):
+        pass
